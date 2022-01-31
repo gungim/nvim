e50fd1748d7165e05498d5755a95e8285ffc5630
0326865837 <0326865837@gmail.com>
2022-01-31 14:30:28 +0700

    my update keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -32,8 +32,11 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<tab>", ":bnext<CR>", opts)
+keymap("n", "<S-tab>", ":bprevious<CR>", opts)
+keymap("n", "<leader>d", ":bdelete<CR>", opts)
+keymap("n", "<F3>",":Autoformat<CR>", opts)
+
 
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
