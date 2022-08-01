5032f6b66564ff1452b45bd78cfffbf5e1b1b259
gungim <gungim>
2022-08-01 14:43:38 +0700

    add gungim_theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,9 @@
+local gungim_theme = require("gungim_theme")
 vim.cmd [[
   set termguicolors
   set background=dark
-  colorscheme github_dimmed
-  " colorscheme palenight
-
+ colorscheme gungim_theme
+  highlight NvimTreeFolderIcon guifg=#84FDFe
 ]]
 
--- gungim_theme.setup({ nvim_tree = { contrast = true } })
-
-require "user.themes.github_theme_config"
+gungim_theme.setup({ nvim_tree = { contrast = true } })
