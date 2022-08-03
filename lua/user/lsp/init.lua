ea27c6324b3ab67bd36ca2222da43bfb441056c0
gungim <gungim>
2022-08-03 16:15:53 +0700

    remove lgv in init

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -7,7 +7,3 @@ require "user.lsp.lsp-installer"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
 require "user.lsp.lsp-colors"
-
-lspconfig.emmet_ls.setup({
-  filetypes = { 'html', '*.js', '*.jsx', '*.ts', '*.tsx', 'css', 'sass', 'scss', 'less' },
-})
