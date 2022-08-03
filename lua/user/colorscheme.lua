5f9bdb3d2bb5e64077bdc347c100251954960312
gungim <gungim>
2022-08-03 16:18:22 +0700

    remove config theme with vim, use lus

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,9 +1,3 @@
 local gungim_theme = require("gungim_theme")
-vim.cmd [[
-  set termguicolors
-  set background=dark
- colorscheme gungim_theme
-  highlight NvimTreeFolderIcon guifg=#84FDFe
-]]
 
 gungim_theme.setup({ nvim_tree = { contrast = true } })
