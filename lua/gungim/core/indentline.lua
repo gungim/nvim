0abb3fc99d4ea54a9fae4bfc9bdeb70786c47c5f
gungim <gungim@github.com>
2023-09-29 09:30:09 +0700

    upgrade indent-blankline to v3

--- lua/gungim/core/indentline.lua
+++ lua/gungim/core/indentline.lua
@@ -1,47 +1,40 @@
 local M = {}
 
 function M.config()
-	gungim.builtin.indentlines = {
-		active = true,
-		on_config_done = nil,
-		options = {
-			enabled = true,
-			show_end_of_line = false,
-			space_char_blankline = " ",
-			show_current_context_start = false,
-			show_current_context = true,
-			use_treesitter = false,
-			show_first_indent_level = true,
-			show_trailing_blankline_indent = false,
-			char = gungim.icons.ui.LineLeft,
-			buftype_exclude = { "terminal", "nofile" },
-			filetype_exclude = {
-				"help",
-				"startify",
-				"dashboard",
-				"lazy",
-				"neogitstatus",
-				"NvimTree",
-				"Trouble",
-				"text",
-			},
-		}
-	}
-end
-
-M.setup = function()
-	local status_ok, indent_blankline = pcall(require, "indent_blankline")
+	local status_ok, ibl = pcall(require, "ibl")
 	if not status_ok then
 		return
 	end
 
-	vim.opt.list = true
-	vim.opt.listchars:append "space:⋅"
-	indent_blankline.setup(gungim.builtin.indentlines.options)
+	local highlight = {
+		"RainbowRed",
+		"RainbowYellow",
+		"RainbowBlue",
+		"RainbowOrange",
+		"RainbowGreen",
+		"RainbowViolet",
+		"RainbowCyan",
+	}
+	local hooks = require("ibl.hooks")
+	-- create the highlight groups in the highlight setup hook, so they are reset
+	-- every time the colorscheme changes
+	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+	end)
 
-	if gungim.builtin.indentlines.on_config_done then
-		gungim.builtin.indentlines.on_config_done()
-	end
+	vim.g.rainbow_delimiters = { highlight = highlight }
+	ibl.setup({
+		indent = { highlight = nil, char = "▏" },
+		scope = { enabled = true, highlight = highlight },
+	})
+
+	hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 end
 
 return M
