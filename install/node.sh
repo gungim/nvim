6d693810cef880bdcb1b9be8a988211d38b2e467
gungim <gungim@github.com>
2023-11-10 16:28:26 +0700

    script install lsp-server

--- install/node.sh
+++ install/node.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-npm install -g @tailwindcss/language-server vscode-langservers-extracted typescript typescript-language-server svelte-language-server stylelint-lsp diagnostic-languageserver bash-language-server @vue/language-server prettier @angular/language-server
+npm install -g @angular/cli @angular/language-server @tailwindcss/language-server @vue/language-server bash-language-server corepack diagnostic-languageserver eas-cli neovim prettier sharp-cli stylelint-lsp svelte-language-server typescript-language-server typescript vscode-langservers-extracted prisma/language-server
